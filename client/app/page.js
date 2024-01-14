63e84fcc88de27da30953a5cfa0eb1bdf4e44873
Tiago Pereira <tiagorp2004@gmail.com>
2024-01-14 21:52:37 +0000

    ffs

--- client/app/page.js
+++ client/app/page.js
@@ -5,14 +5,14 @@ import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [message, setMessage] = useState('Loading');
-  const [people, setPeople] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:8080/api/home');
         const data = await response.json();
         setMessage(data.message);
-        setPeople(data.people);
+        console.log('data catched');
       } catch (error) {
         console.error('Error:', error);
       }
@@ -22,9 +22,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <p className="mb-4">{message}</p>
-      {people.map((name, index) => (
+      {/*people.map((name, index) => (
         <p key={index}>{name}</p>
-      ))}
+      ))*/}
     </main>
   );
 }
--- server/server.py
+++ server/server.py
@@ -3,63 +3,50 @@ from flask_cors import CORS
 from langchain.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.vectorstores import FAISS 
 from dotenv import load_dotenv
 import sys
-print(sys.executable)
 load_dotenv()
-
-embeddings = OpenAIEmbeddings()
-
-
-def create_vector_db_from_youtube_url(video_url: str) -> FAISS:
-    loader = YoutubeLoader.from_youtube_url(video_url)
-    transcript = loader.load()
-    
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    docs = text_splitter.split_documents(transcript)
-    
-    db = FAISS.from_documents(docs, embeddings)
-    return db
-
-def get_response_from_query(db, query, k=4):
-    # 4097
-    
-    docs = db.similarity_search(query, k=k)
-    docs_page_content = " ".join([d.page_content for d in docs])
-    
-    llm = OpenAI(model="text-davinci-003")
-    prompt = PromptTemplate(
-        input_variables=["question", "docs"],
-        template="""You are a helpful YouTube assistant that can awnser questions about videos based on the video's transcript.input_types=
-        
-        Answer the following question: {question}
-        By searching the following video transcript: {docs}
-        
-        Only use the factual information from the transcript to awnser the question. 
-        
-        If you feel like you don't have enough information to awnser the question, say "I don't have sufficient information on that topic in order to awnser that with confidence". 
-        
-        Your awnsers should be detailed. 
-        
-        """)
-    chain = LLMChain(llm = llm, prompt=prompt)
-    response = chain.run(question = query, docs = docs_page_content)
-    response = response.replace("\n", "")
-    return response
 
 app = Flask(__name__)
 CORS(app)
 
+def get_transcript_from_youtube_url() -> str:
+    loader = YoutubeLoader.from_youtube_url("https://youtu.be/uLsAhwJzQoI?si=G0uzqHhtYxiqWLJS")
+    transcript = loader.load()
+    return transcript
+
+def get_response_from_transcript(transcript, query):
+    llm = OpenAI(temperature=1)
+    prompt = PromptTemplate(
+        input_variables=["question", "transcript"],
+        template="""You are a helpful YouTube assistant that can answer questions about videos based on the video's transcript.
+
+        Answer the following question: {question}
+        By searching the following video transcript: {transcript}
+
+        Only use the factual information from the transcript to answer the question. 
+        
+        If you feel like you don't have enough information to answer the question, say "I don't have sufficient information on that topic in order to answer that with confidence". 
+        
+        Your answers should be detailed."""
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    response = chain.run(question=query, transcript=transcript)
+    response = response.replace("\n", "")
+    return response
+
 @app.route("/api/home", methods=["GET"])
 def return_home():
+    question = "What is capitalism?"
+    transcript = get_transcript_from_youtube_url()
+    response = get_response_from_transcript(transcript, question)
+    print(response)
     return jsonify({
-        "message": "Hello Everyone!",
-        "people": ["Tiago", "João", "Dingle"]
+        "message": response
     })
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
